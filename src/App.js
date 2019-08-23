9300a98849e5a670368364059829a05aff555637
ahendroo <ahendroo1@gmail.com>
2019-08-23 12:22:04 +0700

    editing

--- src/App.js
+++ src/App.js
@@ -21,39 +21,7 @@ class App extends Component {
     return (
 
       <div>
-      <header>
-
-          <nav class="bg-navbar navbar navbar-expand-lg navbar-light ">
-
-            {/* <p class="text-white">DISKUUPI</p> */}
-            
-            <div className="container">
-              <div class="navbar-nav mr-auto">
-                <div class="nav-item active">
-                  <a class="nav-link" class="text-white" href="#">{this.state.judul} <span class="sr-only"></span></a>
-                </div>
-              </div>
-            </div>
-
-          </nav>
-
-          <Route exact path="/" component={IndexPage} />
-          <Route exact path="/voucher" component={Voucher} />
-
-      </header>
-
-        <footer class="bg-navbar " align="center">
-          <div className="container p-5">
-            <div class="navbar-nav mr-auto">
-              <div class="nav-item active">
-              <p class="">Made with ❤ in Surabaya</p>
-              </div>
-            </div>
-          </div>
-        </footer>
-
-
-
+          <image src="./img_comingsoon.jpg" width="100%" />
       </div>
       
   );
